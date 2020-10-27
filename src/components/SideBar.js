790b11b312bbdc63b653400f13cad25e5ce3effe
007steve <stephen.plummer1.sp@gmail.com>
2020-10-26 23:31:45 -0400

    added sidebar

---
+++ src/components/SideBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './SideBar.css'
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-input-container">
+                <input placeholder="search"  type="text"/>
+            </div>
+
+            <div className="sidebar-music-container">
+
+            </div>
+            <div className="sidebar-playlist-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default SideBar
